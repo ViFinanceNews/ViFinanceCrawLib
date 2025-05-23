35c8b880f976847a9186f95a428f256b8d742c5d
DTJ-Tran <Dat.TT228006@sis.hust.edu.vn>
2025-05-23 09:27:23 +0700

    Check the AQD line 367 for change the TTL

--- article_database/ArticleQueryDatabase.py
+++ article_database/ArticleQueryDatabase.py
@@ -363,6 +363,7 @@ class AQD:
 
                 if inserted_id: # article_id - Only delete Redis key if DB operation successful
                     session_exists = self.redis_client.exists(session_key)
+
                     
                     if not session_exists: 
                         ttl = self.redis_client.ttl(key)
